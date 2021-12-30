73bd3597f3f8ddba55d5c8f84f9ebd2081337556
zaioti <zaioti@tuta.io>
2021-12-29 23:28:04 -0300

    Pequenas alterações nos arquivos

--- src/commands/info/ping.js
+++ src/commands/info/ping.js
@@ -1,11 +1,11 @@
 module.exports = {
-    name: "ping",
-    aliases: ['p'],
-    run: async (bot, message) => {
-        const m = await message.channel.send("• Ping?");
-        
-        const info = `• Shard: **#${Number(bot.shard.ids) + 1}**\n• Latência do servidor: **${m.createdTimestamp  - message.createdTimestamp}ms**\n• Latência da API: **${Math.round(bot.ws.ping)}ms**`
-        
-        m.edit(info);
-    }
+  name: "ping",
+  aliases: ['p'],
+  run: async (bot, message) => {
+    const m = await message.channel.send("• Ping?");
+
+    const info = `• Shard: **#${Number(bot.shard.ids) + 1}**\n• Latência do servidor: **${m.createdTimestamp  - message.createdTimestamp}ms**\n• Latência da API: **${Math.round(bot.ws.ping)}ms**`;
+
+    m.edit(info);
+  }
 }
